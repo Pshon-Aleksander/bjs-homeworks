17b042ce7115d7fccc65df9c44bfef6fc03c9188
NicolasSev <sevasresident@gmail.com>
2021-06-17 13:46:08 +0600

    homework 1.1

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -6,22 +6,36 @@
  */
 
 function averageMark(a, g, p) {
-	
+
     // реализуйте здесь задание №1
-    
-	return // ваш ответ;
+	let algebra = a
+	let geography = g
+	let physics = p
+
+	let result = ((algebra + geography + physics) / 3)
+
+	return result;
 }
 
 function sayHello(userName) {
-	
+
     // реализуйте здесь задание №2
-    
-	return // ваш ответ;
+	let myName = userName
+	let message = `Привет, мир! Меня зовут ${userName}`
+
+	return message;
 }
 
 function calculateFormula() {
-    
+
 	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+	let x
+	let y
+	let z
+	x = 2
+	y = 22
+	z = 0
+	let result = x * y + 5 * z + x - 1
+
+    return result;
 }
